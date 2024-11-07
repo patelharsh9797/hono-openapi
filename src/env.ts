344fb46d5db836cb4da611aeb7192bbcfe78270d
Harsh <patelharsh9797@gmil.com>
2024-11-07 04:29:41 +0000

    .env & .gitignore updated

--- src/env.ts
+++ src/env.ts
@@ -15,8 +15,8 @@ const EnvSchema = z.object({
   NODE_ENV: z.string().default("development"),
   PORT: z.coerce.number().default(9999),
   LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace", "silent"]),
-//   DATABASE_URL: z.string().url(),
-//   DATABASE_AUTH_TOKEN: z.string().optional(),
+  DATABASE_URL: z.string().url(),
+  DATABASE_AUTH_TOKEN: z.string().optional(),
 }).superRefine((input, ctx) => {
   if (input.NODE_ENV === "production" && !input.DATABASE_AUTH_TOKEN) {
     ctx.addIssue({
